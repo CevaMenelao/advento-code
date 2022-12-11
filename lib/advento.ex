c67804b548af2db8cba65a9775100051e19ed610
Luis Enrique Aguilar Díaz <38533197+CevaMenelao@users.noreply.github.com>
2022-12-10 18:27:48 -0600

    First commit

---
+++ lib/advento.ex
@@ -0,0 +1,18 @@
+defmodule Advento do
+  @moduledoc """
+  Documentation for `Advento`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Advento.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
